7dfe8ce4516cabd781db7e9877a5a3728e8e208f
ashwiniactive <152055899+ashwiniactive@users.noreply.github.com>
2023-12-20 09:32:31 +0530

    Add files via upload

---
+++ Demoset.java
@@ -0,0 +1,32 @@
+package com.tns.collections;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Demoset {
+
+	public static void main(String[] args) {
+
+
+		
+		Set <Integer> value = new TreeSet<>();
+		value.add(20);
+		value.add(20);
+		value.add(30);
+		value.add(120);
+		value.add(50);
+		value.add(9);
+		
+		//Collections.sort(value);
+		
+		Iterator<Integer> i = value.iterator();
+		
+		while (i.hasNext())
+		{
+			System.out.println(i.next());
+		}
+		
+	}
+
+}
